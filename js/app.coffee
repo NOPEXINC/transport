99725f4dc60cc0add8cbbae78f6a72439e45ba27
zechtz <zachariahn@gmail.com>
2014-08-10 16:26:11 +0300

    Adjust mobile breakpoints from 480 to 320

---
+++ js/app.coffee
@@ -0,0 +1,4 @@
+jQuery ->
+	$('[data-transport]').transport ->
+		mobile: '(max-width: 480px)',
+		tablet: '(max-width: 1024px)'
--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('[data-transport]').transport({
-	mobile: '(max-width: 480px)',
-	tablet: '(max-width: 1024px)'
+		mobile: '(max-width: 480px)',
+		tablet: '(max-width: 1024px)'
+	});
 });
-});
-
